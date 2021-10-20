e2372041d0f8075315be8b4e0fda0435f78e80f2
tchassi jordan <tchassijordan@icloud.com>
2021-10-20 15:31:56 +0100

    Updated my portfolio

--- docs/javascript/index.js
+++ docs/javascript/index.js
@@ -3,55 +3,58 @@ const displayAllPortfolio = document.getElementById("d-all");
 const displayWebPorfolio = document.getElementById("d-web");
 const displayGamesPorfolio = document.getElementById("d-games");
 const web = document.querySelectorAll(".web");
-//convert my nodelist into an array
-const portfolioList = Array.from(portfolioItems);
-const form = document.getElementById('form');
+const portfolioList = Array.from(portfolioItems); //convert my nodelist into an array
+let isAll = false;
+let isWebsite = false;
+let isGames = false;
+const root = document.querySelector(":root");
+const rootStyles = getComputedStyle(root);
+const activePortfolioNav = rootStyles.getPropertyValue('--white-gn');
+const inActivePortfolioNav = rootStyles.getPropertyValue('--brownish-white');
 
-displayGamesPorfolio.addEventListener('click', ()=>{
-    for(let i = 0; i < portfolioItems.length; i++) {
-        portfolioItems[i].style.display = "none";
-        if(portfolioItems[i].classList.contains('game'))
-        portfolioItems[i].style.display = "block";
-        if((i === portfolioItems.length - 1) && 
-        portfolioItems[i].classList.contains('game'))
-        handleLastChild(portfolioItems[i]);
+function setInitial() {
+    isAll = false;
+    isWebsite = false;
+    isGames = false;
+    displayAllPortfolio.style.color = inActivePortfolioNav; 
+    displayGamesPorfolio.style.color = inActivePortfolioNav;
+    displayWebPorfolio.style.color = inActivePortfolioNav;
+    portfolioList.map(item => item.style.display = "none");
+}
+
+function handleActivePortfolio() {
+    if (isAll){ 
+        displayAllPortfolio.style.color = activePortfolioNav;
+        portfolioList.map(item => item.style.display = "block");
     }
-});
-
-function handleLastChild(child) {
-    if(window.innerWidth < 500) {
-        portfolioList.forEach(item => {
-            item.style.borderBottom = "1px solid black";
-            item.style.paddingBottom = "1em";
-        })
-        child.style.borderBottom = "none";
-        child.style.paddingBottom = 0;
-        return child;
+    else if(isGames){
+        displayGamesPorfolio.style.color = activePortfolioNav;
+        portfolioList.filter(item => item.classList.contains('game')).map(item => item.style.display = "block");
+        
+    } else if(isWebsite){
+        displayWebPorfolio.style.color = activePortfolioNav;
+        portfolioList.filter(item => item.classList.contains('web')).map(item => item.style.display = "block");
     }
 }
 
+displayGamesPorfolio.addEventListener('click', ()=>{
+    setInitial();
+    isGames = !isGames;
+    handleActivePortfolio();
+});
+
 displayWebPorfolio.addEventListener('click', ()=>{
-    for(let i = 0; i < portfolioItems.length; i++) {
-        portfolioItems[i].style.display = "none";
-        if(portfolioItems[i].classList.contains('web'))
-        portfolioItems[i].style.display = "block";
-        if((i === web.length - 1) && 
-        portfolioItems[i].classList.contains('web'))
-        handleLastChild(portfolioItems[i]);
-    }
+    setInitial();
+    isWebsite = !isWebsite;
+    handleActivePortfolio();
 });
 
 displayAllPortfolio.addEventListener('click', displayAll);
 
 function displayAll() {
-    handleLastChild(portfolioList[portfolioList.length - 1])
-    const allItems = portfolioList.map(item => item.style.display = "block");
-    return allItems;
+    setInitial();
+    isAll = !isAll;
+    handleActivePortfolio();
 }
 
 window.addEventListener('load', displayAll);
-
-form.addEventListener('submit', e => {
-    e.preventDefault()
-    alert('Please get to me using the icons below, contact encountered a malfunction');
-})
