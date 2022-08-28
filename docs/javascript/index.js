ea6e22d8532c853e522cbe305249d3d31b7e3ec1
tchassijordan <tchassijordan@gmail.com>
2022-08-28 22:20:52 +0100

    feat: add Locate to portfolio projects and restructure skills section

--- docs/javascript/index.js
+++ docs/javascript/index.js
@@ -1,8 +1,10 @@
-const portfolioItems = document.querySelectorAll('.portfolio-item');
+const portfolioItems = document.querySelectorAll(".portfolio-item");
 const displayAllPortfolio = document.getElementById("d-all");
 const displayWebPorfolio = document.getElementById("d-web");
 const displayGamesPorfolio = document.getElementById("d-games");
-const portfolioNavLinks = [...document.querySelectorAll('.portfolio-navigation li')];
+const portfolioNavLinks = [
+  ...document.querySelectorAll(".portfolio-navigation li"),
+];
 const web = document.querySelectorAll(".web");
 const portfolioList = Array.from(portfolioItems); //convert portfolio nodelist into an array
 let isAll = false;
@@ -11,7 +13,7 @@ let isGames = false;
 const navLinks = document.querySelector(".nav-bar-links");
 const root = document.querySelector(":root");
 const rootStyles = getComputedStyle(root);
-const activePortfolioNav = rootStyles.getPropertyValue('--white-gn');
+const activePortfolioNav = rootStyles.getPropertyValue("--white-gn");
 const openSlider = document.getElementById("open-slider");
 const closeSlider = document.getElementById("close-slider");
 let navIsActive = false;
@@ -19,13 +21,13 @@ const h = document.querySelector("header");
 const f = document.querySelector("footer");
 let aboutIsActive = false;
 const abtController = document.getElementById("abt-txt");
-const abtNav = document.querySelector('.nav-bar-about-nav');
-const topSettingIcon = document.getElementById('setting-top');
-const topSettingNav = document.querySelector('.setting-top-nav');
+const abtNav = document.querySelector(".nav-bar-about-nav");
+const topSettingIcon = document.getElementById("setting-top");
+const topSettingNav = document.querySelector(".setting-top-nav");
 let settingIsActive = false;
-const bottomSettingBtn = document.getElementById('setting-bottom-btn');
-const bottomSettingBtnIcon = document.querySelector('.setting-bottom-i');
-const bottomSettingNav = document.querySelector('.setting-bottom-nav');
+const bottomSettingBtn = document.getElementById("setting-bottom-btn");
+const bottomSettingBtnIcon = document.querySelector(".setting-bottom-i");
+const bottomSettingNav = document.querySelector(".setting-bottom-nav");
 let settingBelowIsActive = false;
 const setLightTheme = document.getElementById("light-theme");
 const setDarkTheme = document.getElementById("dark-theme");
@@ -34,212 +36,220 @@ const setDarkThemeTop = document.getElementById("dark-theme-top");
 let isLight = false;
 let isDark = false;
 const rs = root.style;
-const locker = [...document.querySelectorAll('.lock')];
+const locker = [...document.querySelectorAll(".lock")];
 
 function closeNav() {
-    h.classList.remove('fullNav');
-    if(window.innerWidth < 726) {
-        navIsActive = false;
-        handleNavbar();
-        setTimeout(()=> {
-            navLinks.style.display  = "none"}, 300);
-    };
-    setTimeout(()=> bottomSettingNav.style.display  = "none", 300);
-    setTimeout(()=> topSettingNav.style.display  = "none", 300);
-    setTimeout(()=> abtNav.style.display  = "none", 300);
-    aboutIsActive = false;
-    settingIsActive = false;
-    settingBelowIsActive = false;
-    handleAboutNav();
-    handleSetting();
+  h.classList.remove("fullNav");
+  if (window.innerWidth < 726) {
+    navIsActive = false;
+    handleNavbar();
+    setTimeout(() => {
+      navLinks.style.display = "none";
+    }, 300);
+  }
+  setTimeout(() => (bottomSettingNav.style.display = "none"), 300);
+  setTimeout(() => (topSettingNav.style.display = "none"), 300);
+  setTimeout(() => (abtNav.style.display = "none"), 300);
+  aboutIsActive = false;
+  settingIsActive = false;
+  settingBelowIsActive = false;
+  handleAboutNav();
+  handleSetting();
 }
 
-locker.forEach(item => item.addEventListener('click', closeNav));
+locker.forEach((item) => item.addEventListener("click", closeNav));
 
-openSlider.addEventListener('click', () => {
-    navIsActive = !navIsActive
-    handleNavbar()  
-})
+openSlider.addEventListener("click", () => {
+  navIsActive = !navIsActive;
+  handleNavbar();
+});
 
-closeSlider.addEventListener('click', () => {
-    navIsActive = !navIsActive
-    handleNavbar()  
-})
+closeSlider.addEventListener("click", () => {
+  navIsActive = !navIsActive;
+  handleNavbar();
+});
 
 function handleNavbar() {
-    if (navIsActive) {
-        navLinks.classList.remove('out');
-        navLinks.classList.add('in');
-        navLinks.style.display = "block";
-        openSlider.style.display = "none";
-        closeSlider.style.display = "block";
-    }
-    else {
-        navLinks.classList.remove('in');
-        navLinks.classList.add('out');
-        setTimeout(()=> navLinks.style.display  = "none", 300);
-        openSlider.style.display = "block";
-        closeSlider.style.display = "none";
-    }
+  if (navIsActive) {
+    navLinks.classList.remove("out");
+    navLinks.classList.add("in");
+    navLinks.style.display = "block";
+    openSlider.style.display = "none";
+    closeSlider.style.display = "block";
+  } else {
+    navLinks.classList.remove("in");
+    navLinks.classList.add("out");
+    setTimeout(() => (navLinks.style.display = "none"), 300);
+    openSlider.style.display = "block";
+    closeSlider.style.display = "none";
+  }
 }
 
 function handleAboutNav() {
-    if(aboutIsActive) {
-        abtController.classList.add('el');
-        abtNav.classList.remove('out');
-        abtNav.classList.add('in');
-        abtNav.style.display = "block";
-    } else {
-        abtController.classList.remove('el');
-        abtNav.classList.remove('in');
-        abtNav.classList.add('out');
-        setTimeout(()=> abtNav.style.display  = "none", 300);
-    }
+  if (aboutIsActive) {
+    abtController.classList.add("el");
+    abtNav.classList.remove("out");
+    abtNav.classList.add("in");
+    abtNav.style.display = "block";
+  } else {
+    abtController.classList.remove("el");
+    abtNav.classList.remove("in");
+    abtNav.classList.add("out");
+    setTimeout(() => (abtNav.style.display = "none"), 300);
+  }
 }
 
-abtController.addEventListener('click', ()=>{
-    aboutIsActive = !aboutIsActive;
-    handleAboutNav();
-})
+abtController.addEventListener("click", () => {
+  aboutIsActive = !aboutIsActive;
+  handleAboutNav();
+});
 
 function handleSetting() {
-    if (settingIsActive) {
-        topSettingIcon.classList.add('el', 'spin');
-        topSettingNav.classList.remove('out');
-        topSettingNav.classList.add('in');
-        topSettingNav.style.display = "block";
-    } else if (!settingIsActive) {
-        topSettingIcon.classList.remove('el', 'spin');
-        topSettingNav.classList.remove('in');
-        topSettingNav.classList.add('out');
-        setTimeout(()=> topSettingNav.style.display  = "none", 300);
-    }
-    
-    if (settingBelowIsActive) {
-        bottomSettingBtn.classList.add('el', 'el-b');
-        bottomSettingBtnIcon.classList.add('spin');
-        bottomSettingNav.classList.remove('out');
-        bottomSettingNav.classList.add('in');
-        bottomSettingNav.style.display = "block";
-    } else if (!settingBelowIsActive) {
-        bottomSettingBtn.classList.remove('el', 'el-b');
-        bottomSettingBtnIcon.classList.remove('spin');
-        bottomSettingNav.classList.remove('in');
-        bottomSettingNav.classList.add('out');
-        setTimeout(()=> bottomSettingNav.style.display  = "none", 300);
-    }
+  if (settingIsActive) {
+    topSettingIcon.classList.add("el", "spin");
+    topSettingNav.classList.remove("out");
+    topSettingNav.classList.add("in");
+    topSettingNav.style.display = "block";
+  } else if (!settingIsActive) {
+    topSettingIcon.classList.remove("el", "spin");
+    topSettingNav.classList.remove("in");
+    topSettingNav.classList.add("out");
+    setTimeout(() => (topSettingNav.style.display = "none"), 300);
+  }
 
-    if (isLight) {
-        portfolioList.map(item => item.classList.add('light'));
-        rs.setProperty('--white', 'var(--nav-bar-black)');
-        rs.setProperty('--nav-txt-col', 'var(--white)');
-        rs.setProperty('--nav-bg', 'var(--brownish-white-nh)');
-        rs.setProperty('--about-bg', 'var(--light-brownish-white)');
-        rs.setProperty('--portfolio-bg', 'whitesmoke');
-        rs.setProperty('--skills-bg', 'var(--light-blue-skl)');
-        rs.setProperty('--footer-bg', 'var(--brownish-white-nh)');
-        rs.setProperty('--setting-bg', 'var(--light-brownish-white)');
-        rs.setProperty('--footer-txt-col', 'var(--nav-bar-black-light)');
-        rs.setProperty('--light-gray', 'rgba(207, 206, 186, 0.288)');
-        rs.setProperty('--article-bg', 'var(--light-gray)');
-        rs.setProperty('--code-bg','rgba(92, 91, 91, 0.747)');
-        f.classList.remove('dark-f');
-        f.classList.add('light-f');
-    } else if (isDark) {
-        portfolioList.map(item => item.classList.remove('light'));
-        rs.setProperty('--white', 'rgb(255, 255, 255');
-        rs.setProperty('--nav-txt-col', 'var(--brownish-white)');
-        rs.setProperty('--nav-bg', 'var(--nav-bar-black)');
-        rs.setProperty('--about-bg', 'var(--light-blue)');
-        rs.setProperty('--portfolio-bg', 'var(--dark-blue)');
-        rs.setProperty('--skills-bg', 'var(--light-gray)');
-        rs.setProperty('--footer-bg', 'var(--nav-bar-black)');
-        rs.setProperty('--setting-bg', 'var(--nav-bar-black-light)');
-        rs.setProperty('--footer-txt-col', 'rgba(255, 255, 255, 0.8)');
-        rs.setProperty('--light-gray', 'rgba(207, 206, 186, 0.062)');
-        rs.setProperty('--article-bg', 'var(--portfolio-bg)');
-        rs.setProperty('--code-bg','');
-        f.classList.remove('light-f');
-        f.classList.add('dark-f');
-    }
+  if (settingBelowIsActive) {
+    bottomSettingBtn.classList.add("el", "el-b");
+    bottomSettingBtnIcon.classList.add("spin");
+    bottomSettingNav.classList.remove("out");
+    bottomSettingNav.classList.add("in");
+    bottomSettingNav.style.display = "block";
+  } else if (!settingBelowIsActive) {
+    bottomSettingBtn.classList.remove("el", "el-b");
+    bottomSettingBtnIcon.classList.remove("spin");
+    bottomSettingNav.classList.remove("in");
+    bottomSettingNav.classList.add("out");
+    setTimeout(() => (bottomSettingNav.style.display = "none"), 300);
+  }
+
+  if (isLight) {
+    portfolioList.map((item) => item.classList.add("light"));
+    rs.setProperty("--white", "var(--nav-bar-black)");
+    rs.setProperty("--nav-txt-col", "var(--white)");
+    rs.setProperty("--nav-bg", "var(--brownish-white-nh)");
+    rs.setProperty("--about-bg", "var(--light-brownish-white)");
+    rs.setProperty("--portfolio-bg", "whitesmoke");
+    rs.setProperty("--skills-bg", "var(--light-blue-skl)");
+    rs.setProperty("--footer-bg", "var(--brownish-white-nh)");
+    rs.setProperty("--setting-bg", "var(--light-brownish-white)");
+    rs.setProperty("--footer-txt-col", "var(--nav-bar-black-light)");
+    rs.setProperty("--light-gray", "rgba(207, 206, 186, 0.288)");
+    rs.setProperty("--article-bg", "var(--light-gray)");
+    rs.setProperty("--code-bg", "rgba(92, 91, 91, 0.747)");
+    f.classList.remove("dark-f");
+    f.classList.add("light-f");
+  } else if (isDark) {
+    portfolioList.map((item) => item.classList.remove("light"));
+    rs.setProperty("--white", "rgb(255, 255, 255");
+    rs.setProperty("--nav-txt-col", "var(--brownish-white)");
+    rs.setProperty("--nav-bg", "var(--nav-bar-black)");
+    rs.setProperty("--about-bg", "var(--light-blue)");
+    rs.setProperty("--portfolio-bg", "var(--dark-blue)");
+    rs.setProperty("--skills-bg", "var(--light-gray)");
+    rs.setProperty("--footer-bg", "var(--nav-bar-black)");
+    rs.setProperty("--setting-bg", "var(--nav-bar-black-light)");
+    rs.setProperty("--footer-txt-col", "rgba(255, 255, 255, 0.8)");
+    rs.setProperty("--light-gray", "rgba(207, 206, 186, 0.062)");
+    rs.setProperty("--article-bg", "var(--portfolio-bg)");
+    rs.setProperty("--code-bg", "");
+    f.classList.remove("light-f");
+    f.classList.add("dark-f");
+  }
 }
 
-topSettingIcon.addEventListener('click', ()=>{
-    settingIsActive = !settingIsActive;
-    handleSetting();
+topSettingIcon.addEventListener("click", () => {
+  settingIsActive = !settingIsActive;
+  handleSetting();
 });
 
-bottomSettingBtn.addEventListener('click' , () => {
-    settingBelowIsActive = !settingBelowIsActive;
-    handleSetting();
+bottomSettingBtn.addEventListener("click", () => {
+  settingBelowIsActive = !settingBelowIsActive;
+  handleSetting();
 });
 
-setDarkTheme.addEventListener('click', ()=> {
-    isLight = false;
-    isDark = !isLight;
-    handleSetting();
+setDarkTheme.addEventListener("click", () => {
+  isLight = false;
+  isDark = !isLight;
+  handleSetting();
 });
 
-setLightTheme.addEventListener('click', ()=> {
-    isDark = false;
-    isLight = !isLight;
-    handleSetting();
+setLightTheme.addEventListener("click", () => {
+  isDark = false;
+  isLight = !isLight;
+  handleSetting();
 });
 
-setDarkThemeTop.addEventListener('click', ()=> {
-    isLight = false;
-    isDark = !isLight;
-    handleSetting();
+setDarkThemeTop.addEventListener("click", () => {
+  isLight = false;
+  isDark = !isLight;
+  handleSetting();
 });
 
-setLightThemeTop.addEventListener('click', ()=> {
-    isDark = false;
-    isLight = !isLight;
-    handleSetting();
+setLightThemeTop.addEventListener("click", () => {
+  isDark = false;
+  isLight = !isLight;
+  handleSetting();
 });
 
 function setInitial() {
-    aboutIsActive = false;
-    isAll = false;
-    isWebsite = false;
-    isGames = false;
-    portfolioNavLinks.map(item => item.style.color = 'var(--nav-txt-col)');
-    portfolioList.map(item => item.style.display = "none");
+  aboutIsActive = false;
+  isAll = false;
+  isWebsite = false;
+  isGames = false;
+  portfolioNavLinks.map((item) => (item.style.color = "var(--nav-txt-col)"));
+  portfolioList.map((item) => (item.style.display = "none"));
 }
 
 function handleActivePortfolio() {
-    if (isAll) {
-        displayAllPortfolio.style.color = activePortfolioNav;
-        portfolioList.map(item => item.style.display = "block");
-    }
-    else if (isGames) {
-        displayGamesPorfolio.style.color = activePortfolioNav;
-        portfolioList.filter(item => item.classList.contains('game')).map(item => item.style.display = "block");
-
-    } else if (isWebsite) {
-        displayWebPorfolio.style.color = activePortfolioNav;
-        portfolioList.filter(item => item.classList.contains('web')).map(item => item.style.display = "block");
-    }
+  if (isAll) {
+    displayAllPortfolio.style.color = activePortfolioNav;
+    portfolioList.map((item) => (item.style.display = "block"));
+  } else if (isGames) {
+    displayGamesPorfolio.style.color = activePortfolioNav;
+    portfolioList
+      .filter((item) => item.classList.contains("game"))
+      .map((item) => (item.style.display = "block"));
+  } else if (isWebsite) {
+    displayWebPorfolio.style.color = activePortfolioNav;
+    portfolioList
+      .filter((item) => item.classList.contains("web"))
+      .map((item) => (item.style.display = "block"));
+  }
 }
 
-displayGamesPorfolio.addEventListener('click', () => {
-    setInitial();
-    isGames = !isGames;
-    handleActivePortfolio();
+displayGamesPorfolio.addEventListener("click", () => {
+  setInitial();
+  isGames = !isGames;
+  handleActivePortfolio();
 });
 
-displayWebPorfolio.addEventListener('click', () => {
-    setInitial();
-    isWebsite = !isWebsite;
-    handleActivePortfolio();
+displayWebPorfolio.addEventListener("click", () => {
+  setInitial();
+  isWebsite = !isWebsite;
+  handleActivePortfolio();
 });
 
-displayAllPortfolio.addEventListener('click', displayAll);
+displayAllPortfolio.addEventListener("click", displayAll);
 
 function displayAll() {
-    setInitial();
-    isAll = !isAll;
-    handleActivePortfolio();
+  setInitial();
+  isAll = !isAll;
+  handleActivePortfolio();
 }
 
-window.addEventListener('load', displayAll);
+function displayWebsite() {
+  setInitial();
+  isWebsite = !isWebsite;
+  handleActivePortfolio();
+}
+
+window.addEventListener("load", displayWebsite);
